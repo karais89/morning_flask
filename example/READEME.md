24e87e6b2e3f7314da60f737235f228cb158c8d8
kaya <kaya@treenod.com>
2021-04-12 13:25:35 +0900

    세팅

---
+++ example/READEME.md
@@ -0,0 +1,10 @@
+# SqlAlchemy
+
+# Flask migrate
+- https://flask-migrate.readthedocs.io/en/latest/
+
+```
+flask db init
+flask db migrate -m "Initial migration."
+flask db upgrade
+```
--- example/app.py
+++ example/app.py
@@ -1,10 +1,22 @@
 # save this as app.py
 from flask import Flask, escape, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
 
 
 @app.route("/")
 def hello():
     name = request.args.get("name", "World")
     return f"Hello, {escape(name)}!"
+
