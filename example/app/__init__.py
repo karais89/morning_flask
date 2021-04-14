6b57bfc02998ed16f1661de0ac143ac9b676dde2
kaya <kaya@treenod.com>
2021-04-15 08:54:56 +0900

    커밋

--- example/app/__init__.py
+++ example/app/__init__.py
@@ -6,12 +6,15 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
+
+    from app.models import User, UserGameCenterStage
 
     @app.route("/")
     def hello():
--- example/app/models.py
+++ example/app/models.py
@@ -9,3 +9,10 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User %r>" % self.username
+
+
+class UserGameCenterStage(db.Model):
+    sno = db.Column(db.String(80), primary_key=True)
+    version = db.Column(db.Integer, primary_key=True)
+    stageIndex = db.Column(db.Integer, primary_key=True)
+    bestScore = db.Column(db.Integer, nullable=False)
